5726bc8b00cc81c1b8683e7729ae12a66245fbdf
ZlayaMorda <tarus01@mail.ru>
2022-07-11 14:26:56 +0300

    review without await

---
+++ test/Main_test.js
@@ -0,0 +1,48 @@
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+describe("Main", function() {
+    let owner
+    let inviter
+    let partner
+    let main
+
+    before(async function() {
+        [owner, inviter, partner] = await ethers.getSigners()
+        const Main = await ethers.getContractFactory("Main", owner)
+        main = await Main.deploy()
+        await  main.deployed()
+    })
+
+    it("Correct deploy address", async function() {
+        await expect(main.address).to.be.properAddress
+    })
+
+    describe("Empty addresses", function() {
+
+        it("should return empty inviter address", async function() {
+            expect(await main.getInviter(partner.address)).to.hexEqual("0x0")
+        })
+
+        it("should return empty partner address", async function() {
+            const partners_mas = await main.getDirectPartners(inviter.address)
+            expect(await partners_mas.length).equal(0)
+        })
+    })
+
+    describe("Get valid address", function() {
+
+        beforeEach(async function() {
+            await main.connect(partner).referralInvite(inviter.address)
+        })
+
+        it("should return correct inviter address", async function() {
+            expect(await main.getInviter(partner.address)).to.equal(inviter.address)
+        })
+
+        it("should return correct partner address", async function() {
+            const partners_mas = await main.getDirectPartners(inviter.address)
+            expect(await partners_mas[0]).to.equal(partner.address)
+        })
+    })
+})
